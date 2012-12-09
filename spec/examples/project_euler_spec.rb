0fc7eeb7f4b0c756fc6e21eb730e3e39d05840c8
Utkarsh Kukreti <utkarshkukreti@gmail.com>
2012-12-09 20:42:58 +0530

    Missed 2 spaces.

--- spec/examples/project_euler_spec.rb
+++ spec/examples/project_euler_spec.rb
@@ -9,8 +9,8 @@ describe Subtle do
       end
 
       describe "Problem 6:" +
-        "Find the difference between the sum of the squares of the first" +
-        "one hundred natural numbers and the square of the sum." do
+        " Find the difference between the sum of the squares of the first" +
+        " one hundred natural numbers and the square of the sum." do
         e "((+/!101)^2)-+/!101^2", 25164150
       end
     end
