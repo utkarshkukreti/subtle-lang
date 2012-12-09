31655604062b785fb1051a7fbb4676a31b126bb1
Utkarsh Kukreti <utkarshkukreti@gmail.com>
2012-12-09 20:33:13 +0530

    Subtle can now solve Project Euler Problem #6.

--- spec/examples/project_euler_spec.rb
+++ spec/examples/project_euler_spec.rb
@@ -7,6 +7,12 @@ describe Subtle do
         " Find the sum of all the multiples of 3 or 5 below 1000." do
         e "+/&~&/!1000%/:3 5", 233168
       end
+
+      describe "Problem 6:" +
+        "Find the difference between the sum of the squares of the first" +
+        "one hundred natural numbers and the square of the sum." do
+        e "((+/!101)^2)-+/!101^2", 25164150
+      end
     end
   end
 end
